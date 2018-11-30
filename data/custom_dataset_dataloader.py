28c16bf7f0361908ac02b8a502da95ada0633f2d
Qiang Wen <31679296+csqiangwen@users.noreply.github.com>
2018-11-30 20:00:13 +0800

    Create custom_dataset_dataloader.py

---
+++ data/custom_dataset_dataloader.py
@@ -0,0 +1,50 @@
+import torch.utils.data
+from data.custom_dataset import CustomDataset
+
+class BaseDataLoader():
+    def __init__(self):
+        pass
+
+    def initialize(self, opt):
+        self.opt = opt
+        pass
+
+    def load_data(self):
+        return None
+
+def CreateDataset(opt):
+    dataset = None
+    dataset = CustomDataset()
+    dataset.initialize(opt)
+    return dataset
+
+
+class CustomDatasetDataLoader(BaseDataLoader):
+    def name(self):
+        return 'CustomDatasetDataLoader'
+
+    def initialize(self, opt):
+        BaseDataLoader.initialize(self, opt)
+        self.dataset = CreateDataset(opt)
+        self.dataloader = torch.utils.data.DataLoader(
+            self.dataset,
+            batch_size=opt.batchSize,
+            shuffle=True,
+            num_workers=2)
+
+    def load_data(self):
+        return self
+
+    def __len__(self):
+        return len(self.dataset)
+
+    def __iter__(self):
+        for i, data in enumerate(self.dataloader):
+            yield data
+
+def CreateDataLoader(opt):
+    data_loader = CustomDatasetDataLoader()
+    print(data_loader.name())
+    data_loader.initialize(opt)
+    return data_loader
+
